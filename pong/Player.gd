d54d1407842d29c665716935ea9a45ef60967503
Darian | Praxis <hello@vulpinecat.com>
2018-12-03 15:00:24 +0100

    creating a new player scene that looks/feels nicer

--- pong/Player.gd
+++ pong/Player.gd
@@ -1,25 +1,20 @@
-extends Area2D
+extends KinematicBody2D
 
-export (int) var speed
-export (int) var clampOffset
-export (Vector2) var startPos
 var screensize
+export (Vector2) var startPos
 
 func _ready():
 	screensize = get_viewport_rect().size
-	position = startPos
+
 	
-func _process(delta):
-	var velocity = Vector2()
+func _physics_process(delta):
+	var velocity = Vector2(0, 0)
 	
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if velocity.length() > 0:
-		velocity = velocity * speed
-	
-	position += velocity * delta
-	position.y = clamp(position.y, clampOffset, screensize.y - clampOffset)
-	
+		velocity.y = -350
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 350
+		
+
+	move_and_slide(velocity)
 	
