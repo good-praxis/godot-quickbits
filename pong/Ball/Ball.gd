0d9f836cf8958f00b12e9f552fe431361d1eaef3
Darian | Praxis <hello@vulpinecat.com>
2018-12-06 17:53:36 +0100

    adding score counter

--- pong/Ball/Ball.gd
+++ pong/Ball/Ball.gd
@@ -1,9 +1,24 @@
 extends Area2D 
 
-var velocity = 400
+var velocity
 var direction = Vector2()
 var is_colliding = false
+var spawn_position
 
+signal score_point(winner)
+
+func _ready():
+	spawn_position = position
+	velocity = 400
+	randomize()
+	direction = get_rand_direction()
+
+func reset():
+	position = spawn_position
+	velocity = 400
+	randomize()
+	direction = get_rand_direction()
+	
 
 func get_rand_direction():
 	var direction = Vector2()
@@ -17,9 +32,6 @@ func get_rand_direction():
 func set_direction(direction):
 	direction = direction.normalized()
 	
-func _ready():
-	randomize()
-	direction = get_rand_direction()
 	
 func _process(delta):
 	position += direction * velocity * delta
@@ -34,6 +46,11 @@ func _on_Ball_area_entered(area):
 		var collision_vector = position - area.position
 		velocity += 10
 		direction = collision_vector.normalized()
+	elif area.TYPE == "GOAL":
+		if area.position.x < position.x:
+			emit_signal("score_point", "Enemy")
+		else:
+			emit_signal("score_point", "Player")
 	else:
 		direction.y = -direction.y
 
--- pong/Main.gd
+++ pong/Main.gd
@@ -1,4 +1,21 @@
 extends Node
 
+var player_score = 0
+var enemy_score = 0
+
 func _process(delta):
-	$Enemy.set_ball_position($Ball.position)
+	if $Ball:
+		$Enemy.set_ball_position($Ball.position)
+
+
+func _on_Ball_score_point(winner):
+	if winner == "Player":
+		player_score += 1
+	else:
+		enemy_score += 1
+	update_scores()
+	$Ball.reset()
+
+func update_scores():
+	$Score/PlayerScore.text = str(player_score)
+	$Score/EnemyScore.text = str(enemy_score)
